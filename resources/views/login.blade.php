da14f746f4fb5cb3424dd3caf86e193b473b527d
baguseka268 <baguseka268@gmail.com>
2021-10-09 01:10:51 +0700

    login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div class="container">
+    <div class="form-login justify-content-between pt-5 row">
+        <div class="card col-7">
+            <div class="card-body">
+                <div class="form-login">
+                    <form>
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">Email address</label>
+                          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                          <label for="exampleInputPassword1">Password</label>
+                          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="form-check">
+                          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </form>
+                </div>
+            </div>
+          </div>
+
+        <div class="navbar-login col-4">
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+            iki konten navbar jamput
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('header', function () {
     return view('header');
 });
+
+Route::get('login', function () {
+    return view('login');
+});
